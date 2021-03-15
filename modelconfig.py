f28f9c7abca6724102bb0daf5da773557ab43c64
Sachin Goyal <sach.goyalsachin@gmail.com>
2021-03-16 02:15:53 +0530

    Add files via upload

--- modelconfig.py
+++ modelconfig.py
@@ -2,7 +2,7 @@ import collections
 
 config = collections.namedtuple("config", [
     "cnn_channels", "rnn_hidden_size",
-    "lr", "rank", "islstm", "isBi","inp_size",
+    "lr", "rank", "islstm", "isBi", "eps_factor",
 ])
 
 def RC01():
@@ -14,13 +14,12 @@ def RC01():
                   isBi=True)
 
 def RC02():
-    return config(cnn_channels=400,
-                  rnn_hidden_size=200,
+    return config(cnn_channels=600,
+                  rnn_hidden_size=300,
                   lr = 0.001,
-                  rank=50,
+                  rank=32,
                   islstm=True,
-                  isBi=True,
-                  inp_size=32)
+                  isBi=True)
 
 def RC03():
     return config(cnn_channels=400,
@@ -29,4 +28,22 @@ def RC03():
                   rank=50,
                   islstm=True,
                   isBi=True,
-                  inp_size=64)
+                  eps_factor=0.01)
+
+def RC04():
+    return config(cnn_channels=400,
+                  rnn_hidden_size=200,
+                  lr = 0.001,
+                  rank=50,
+                  islstm=True,
+                  isBi=True,
+                  eps_factor=0.03)
+
+def RC05():
+    return config(cnn_channels=400,
+                  rnn_hidden_size=200,
+                  lr = 0.001,
+                  rank=50,
+                  islstm=True,
+                  isBi=True,
+                  eps_factor=0.05)
